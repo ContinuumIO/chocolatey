b4498ce028e8962bf56e8ec11ad7a0f94499fbc3
Michael Sarahan <msarahan@gmail.com>
2016-04-21 08:33:44 -0500

    incorporate VS build tool update from @jordigg

--- VisualCppBuildTools/Tools/ChocolateyInstall.ps1
+++ VisualCppBuildTools/Tools/ChocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'VSModules.psm1')
 
-Install-VS 'VisualCppBuildTools' 'https://download.microsoft.com/download/3/8/E/38EE4758-7C31-4D96-8FF9-83CC313F0F78/VisualCppBuildTools_Full.exe'
+Install-VS 'VisualCppBuildTools' 'http://download.microsoft.com/download/1/f/e/1fe5d584-c5a9-4aee-907a-421abe4c74f2/buildtools_full.exe'
